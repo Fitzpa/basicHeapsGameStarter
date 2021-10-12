af88d908538214c94af51df8ff94cd5434680590
Louie Fitzpatrick <fitz4492@gmail.com>
2021-10-11 23:46:57 -0500

    Initialize advancedBase

---
+++ src/game/Types.hx
@@ -0,0 +1,19 @@
+/**	This enum is used by the Controller class to bind general game actions to actual keyboard keys or gamepad buttons. **/
+enum GameAction {
+	MoveX;
+	MoveY;
+
+	Jump;
+	Restart;
+	Cancel;
+
+	DebugTurbo;
+	DebugSlowMo;
+}
+
+enum Affect {
+	Stun;
+}
+
+enum LevelMark {
+}
